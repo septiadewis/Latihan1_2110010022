96a0777fc04820af028370ae04d9c5c6f164f14f
septia <dewisantikaseptia@gmail.com>
2023-06-12 13:48:08 +0800

    Membuat constructor dan setPenjumlahan

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -10,5 +10,19 @@ package Hitung;
  * @author ASUS
  */
 public class Matematika {
+    //membuat variabel
+    private double bil1, bil2;
+    
+    //membuat constructor
+    public Matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+         
+    }
+    //membuat method setPenjumlahan
+   double setPenjumlahan(){
+       return bil1 + bil2;
+   }
+            
     
 }
