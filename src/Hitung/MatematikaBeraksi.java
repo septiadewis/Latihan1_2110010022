318ff2c568269e63451770b543ba567c5abb9b67
septia <dewisantikaseptia@gmail.com>
2023-06-12 14:08:03 +0800

    Membuat class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -10,5 +10,14 @@ package Hitung;
  * @author ASUS
  */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args){
+        //membuat objek
+        Matematika septia = new Matematika(2,2);
+        
+        System.out.println("Hasil penjumlahan"+septia.setPenjumlahan());
+        System.out.println("Hasil pengurangan"+septia.setPengurangan());
+        System.out.println("Hasil perkalian"+septia.setPerkalian());
+        System.out.println("Hasil pembagian"+septia.setPembagian());
+    }
+            
 }
